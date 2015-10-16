7cc860c0db0b7ceeb47a07932de9b2ed3f2a29bf
tzw-madzia <madzia.nowak.pl@gmail.com>
2015-10-16 14:35:15 +0200

    Remove this shit

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,4 @@
 class CardsController < ApplicationController
-  def index
-    @card_list = CardList.find(params[:card_list_id])
-    @cards = @card_list.cards
-  end
-
   def import
     Card.import(params[:file],params[:card_list_id])
     @card_list = CardList.find(params[:card_list_id])
