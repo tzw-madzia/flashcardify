08341fc07b5a88e6c9d37089f2349cff213fb82c
tzw-madzia <madzia.nowak.pl@gmail.com>
2016-09-23 14:05:13 +0200

    Add destroy action to cards controller [#100370324]

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -21,6 +21,14 @@ class CardsController < ApplicationController
     end
   end
 
+  def destroy
+    @card_list = CardList.find(params[:card_list_id])
+    @card = Card.find(params[:id])
+    @card.destroy
+    flash[:notice] = 'Card deleted!'
+    redirect_to card_list_path(@card_list.id)
+  end
+
   def card_params
     params.require(:card).permit(:primary_language_word, :learning_language_word, :special_character_word)
   end
