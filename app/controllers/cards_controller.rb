943ef480465c6c7a0bc14ce4ec61a95376e22fd5
tzw-madzia <madzia.nowak.pl@gmail.com>
2016-09-23 13:33:23 +0200

    Add new and create actions to cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,4 +4,24 @@ class CardsController < ApplicationController
     @card_list = CardList.find(params[:card_list_id])
     redirect_to card_list_path(@card_list), notice: "Cards imported."
   end
+
+  def new
+    @card_list = CardList.find(params[:card_list_id])
+    @card = @card_list.cards.new
+  end
+
+  def create
+    @card_list = CardList.find(params[:card_list_id])
+    @card = @card_list.cards.build(card_params)
+    if @card.save
+      flash[:notice] = 'Card added!'
+      redirect_to card_list_path(@card_list.id)
+    else
+      render :new
+    end
+  end
+
+  def card_params
+    params.require(:card).permit(:primary_language_word, :learning_language_word, :special_character_word)
+  end
 end
